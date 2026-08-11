b9f3fc5187788ab385d76e92a1b09af066a8968a
josephvanderharst <josephvanderharst@gmail.com>
2026-08-11 17:44:23 -0400

    Not a fan of hash routers, but it'll have to do.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createHashRouter, RouterProvider } from 'react-router';
 import routes from './routes.tsx';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
