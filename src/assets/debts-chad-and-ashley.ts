781758ef8d2206c66252ea17a804dbc018e9abf1
josephvanderharst <josephvanderharst@gmail.com>
2026-08-11 16:46:08 -0400

    Added Chad & Ashley debt load example.

---
+++ src/assets/debts-chad-and-ashley.ts
@@ -0,0 +1,58 @@
+import type { Debt } from "../types/debt"
+
+export const DEBTS: Debt[] = [
+  {
+    name: 'Motorcycle',
+    amount: 7791.04,
+    interestRate: 22,
+    minPayment: null,
+  },
+  {
+    name: 'Discover',
+    amount: 5843.12,
+    interestRate: 25,
+    minPayment: null,
+  },
+  {
+    name: 'Truck',
+    amount: 3134.93,
+    interestRate: null,
+    minPayment: 400,
+  },
+  {
+    name: 'Tires',
+    amount: 403.00,
+    interestRate: 10,
+    minPayment: null,
+  },
+  {
+    name: 'Car',
+    amount: 9485.86,
+    interestRate: 15,
+    minPayment: 500,
+  },
+  {
+    name: 'HOA',
+    amount: 850.00,
+    interestRate: 17,
+    minPayment: null,
+  },
+  {
+    name: 'Ambulance',
+    amount: 1000.00,
+    interestRate: null,
+    minPayment: 100,
+  },
+  {
+    name: 'Bed',
+    amount: 2855.16,
+    interestRate: 16,
+    minPayment: null,
+  },
+  {
+    name: 'JSC',
+    amount: 6644.77,
+    interestRate: 15,
+    minPayment: null,
+  },
+];
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,11 @@ import { type RouteObject } from "react-router";
 import App from "./pages/App";
 import Home from "./pages/Home";
 import Simulator from "./pages/Simulator";
+import { DEBTS } from "./assets/debts-chad-and-ashley";
 
 export default [
   { path: '/', element: <Home /> },
   { path: '/foo', element: <App /> },
   { path: '/simulator', element: <Simulator /> },
+  { path: '/simulator/chad-and-ashley', element: <Simulator initDebts={DEBTS} /> },
 ] satisfies RouteObject[];
